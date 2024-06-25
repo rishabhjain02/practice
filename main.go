03b0a5d94c94c31f2cdaf9ee764b6cc381c63586
rishabh <rishu1998jain@gmail.com>
2024-06-25 16:37:49 +0530

    some main changes and todo comments

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/rishabh/practice/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rishabh/practice/repository"
+	"github.com/rishabh/practice/usecase"
+)
+
 // We would like you to create a crypto notification app. The features it should include:
 
 // Create a server which will be able to take in the following rest APIs
@@ -10,5 +18,10 @@ package main
 // Delete a notification
 
 func main() {
-	// notificationRepo := repository.New
+	notificationRepo := repository.NewNotificationRepo("db")
+	notificationUsecase := usecase.NewNotificationUsecase(notificationRepo)
+	router := gin.Default()
+	http.NewNotificationHandler(router, *notificationUsecase)
+
+	router.Run(":8080")
 }
--- repository/notification_repository.go
+++ repository/notification_repository.go
@@ -4,6 +4,13 @@ type NotificationRepoStruct struct {
 	db string
 }
 
-func NewNotificationRepo(db string) {
-
+func NewNotificationRepo(db string) NotificationRepo {
+	return nil
 }
+
+// To Do
+
+// Creating connection to db
+// Function to create entry of the notification in db
+// Function to Get all the notifications from the db
+// Function to Delete the notification from the db
